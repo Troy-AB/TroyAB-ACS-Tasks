e448c4da45ec72c46a879bb0dc26aa202ecdbfd5
Troy-AB <lapisz.king@gmail.com>
2023-01-11 18:28:19 +0000

    rewriting space invaders #1

---
+++ PyGame/Space Invaders/Space Invaders Completed/player.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        #inherits data from pygame.sprite.Sprite
+        super().__init__()
+        
+        self.image =
+        self.rect 
