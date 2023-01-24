1f5c2f7ec4fb67b63f46e69eb3914e5c297ba331
Troy-AB <lapisz.king@gmail.com>
2023-01-24 13:31:56 +0000

    aliens

--- PyGame/Space Invaders/Space Invaders Completed/code/Game.py
+++ PyGame/Space Invaders/Space Invaders Completed/code/Game.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from player import Player
 import obstacle
+from alien import Alien
 
 class Game:
     def __init__(self):
@@ -12,16 +13,35 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start= screen_width / 15, y_start= 480, )
 
-    def create_obstacle(self):
+        # alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_setup(rows = 6, cols = 8)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
-            for column_index, column in enumerate(row):
-                if columnm == 'x':
-                    x = column_index * self.block_size
-                    y = row_index * self.block_size
-                    block = obstacle.Block(self.block_size, (241,79,800), x, y)
+            for col_index, col in enumerate(row):
+                if col == 'x':
+                    x = x_start + (col_index * self.block_size) + offset_x
+                    y = y_start + (row_index * self.block_size)
+                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
                     self.blocks.add(block)
-        
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start, ):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
+
+    def alien_setup(self, rows, cols):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index
+                y = row_index
+                alien_sprite = Alien('red', x, y)
+                self.aliens.add(alient_sprite)
+            
 
     def run(self):
         #update all sprite groups
@@ -32,6 +52,7 @@ class Game:
         self.player.draw(screen)
 
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
 
 if __name__ == '__main__':
     pygame.init()
