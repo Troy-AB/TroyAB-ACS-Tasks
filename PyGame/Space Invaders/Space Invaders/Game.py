24e230d2e83640953c477ca9e82aa4d214f9617c
Troy-AB <113181652+Troy-AB@users.noreply.github.com>
2023-01-26 21:48:18 +0000

    Finished Space Invaders

--- PyGame/Space Invaders/Space Invaders/Game.py
+++ PyGame/Space Invaders/Space Invaders/Game.py
@@ -11,6 +11,14 @@ class Game:
         player_sprite = Player((screen_width / 2 , screen_height), screen_width,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+        #health and score set up
+        self.lives = 3
+        self.live_surf = pygame.image.load('player.png').convert_alpha()
+        self.live_x_start_pos = screen_width - (self.live_surf.get_size()[0] * 2 + 20)
+        self.score = 0 
+        self.font = pygame.font.Font("Pixeled.ttf", 20)
+
+
         #obstacle setup
         self.shape = obstacle.shape
         self.block_size = 6
@@ -79,7 +87,61 @@ class Game:
         if self.extra_spawn_time <= 0:
             self.extra.add(Extra(choice(['right', 'left']), screen_width))
             self.extra_spawn_time = randint(400,800)
-            
+
+    def collision_checks(self):
+        #player lasers
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                #obstacle collision
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+
+                #alien collisions
+                aliens_hit = pygame.sprite.spritecollide(laser, self.aliens, True)
+                if aliens_hit:
+                    for alien in aliens_hit:
+                        self.score += alien.value  
+                    laser.kill()
+
+
+                #extra collision
+                if pygame.sprite.spritecollide(laser, self.extra, True):
+                    self.score += 500
+                    laser.kill()
+
+        #alien lasers
+        if self.alien_lasers:
+            for laser in self.alien_lasers:
+                #obstacle collision
+                if pygame.sprite.spritecollide(laser, self.blocks, True):
+                    laser.kill()
+                
+                #player collision
+                if pygame.sprite.spritecollide(laser, self.player, False):
+                    laser.kill()
+                    self.lives -= 1 
+                    if self.lives <= 0:
+                        pygame.quit()
+                        sys.exit()
+
+        #aliens
+        if self.aliens:
+            for alien in self.aliens:
+                pygame.sprite.spritecollide(alien, self.blocks, True)
+
+                if pygame.sprite.spritecollide(alien, self.player, False):
+                    pygame.quit()
+                    sys.exit()
+                    
+    def display_lives(self):
+        for live in range(self.lives - 1):
+            x = self.live_x_start_pos + (live * (self.live_surf.get_size()[0] + 10))
+            screen.blit(self.live_surf,(x,8))
+
+    def display_score(self):
+        score_surf = self.font.render(f'score: {self.score}', False, 'white')
+        score_rect = score_surf.get_rect(topleft = (10,-10))
+        screen.blit(score_surf, score_rect)
 
     def run(self):
         #update all sprite groups
@@ -90,14 +152,16 @@ class Game:
         self.alien_lasers.update()
         self.extra_alien_timer()
         self.extra.update()
+        self.collision_checks()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
-
         self.blocks.draw(screen)
         self.aliens.draw(screen)
         self.alien_lasers.draw(screen)
         self.extra.draw(screen)
+        self.display_lives()
+        self.display_score()
 
 if __name__ == '__main__':
     pygame.init()
